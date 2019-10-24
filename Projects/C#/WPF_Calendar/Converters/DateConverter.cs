cf5855b49ef15d7ec4e456adcdd66a7e33829a4c
LYNE14 <44123587+LYNE14@users.noreply.github.com>
2019-10-24 22:56:41 +0200

    Create DateConverter.cs

---
+++ Projects/C#/WPF_Calendar/Converters/DateConverter.cs
@@ -0,0 +1,34 @@
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WPF_Calendar.Converters
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime date = (DateTime) value;
+
+            string param = (string) parameter;
+
+            switch(param.ToUpper())
+            {
+                case "MONTH":
+                    return date.Month;
+                case "YEAR":
+                    return date.Year;
+                case "DAY":
+                    return date.Day;
+                default:
+                    return date.ToString();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
